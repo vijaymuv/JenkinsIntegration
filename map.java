a32d01c5eb81a4d8d32e51e67319bf14e4553b1c
Vijay <110091571+vijaymuv@users.noreply.github.com>
2023-06-26 18:03:48 +0530

    added mat file

---
+++ map.java
@@ -0,0 +1,9 @@
+public class map {
+    public static void main(String[] args) {
+        // Two dimensional array of items
+        String[][] arrayItems = {{"Vijay ", "Item0","one","first"},
+                {"key1", "Item1","two","last"},
+};
+        System.out.println(arrayItems[0][0].toString());
+    }
+    }
